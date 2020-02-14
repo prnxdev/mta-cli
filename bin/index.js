30939f59f408dbb5b4946d994a41248f577a9769
Pawel Miczka <piorunx@gmail.com>
2020-02-14 19:11:24 +0100

    up: added sending request to MTA HTTP server to restart resource

--- bin/index.js
+++ bin/index.js
@@ -5,25 +5,31 @@ const path = require('path');
 const process = require('process');
 const chokidar = require('chokidar');
 const program = require('commander');
+const axios = require('axios');
 
 program
-    .command('watch')
-    .action(() => {
-        let resourceName = path.basename(path.resolve(process.cwd()));
-        let options = {
-            ignored: ['node_modules'],
-            ignoreInitial: true
-        };
+	.command('watch')
+	.action(() => {
+		let resourceName = path.basename(path.resolve(process.cwd()));
+		let options = {
+			ignored: ['node_modules'],
+			ignoreInitial: true
+		};
 
-        if (!fs.existsSync('./meta.xml')) throw Error('This is not resource folder. No meta.xml file found!');
-       console.log(`Watching changes for "${resourceName}" resource.`);
+		if (!fs.existsSync('./meta.xml')) throw Error('This is not resource folder. No meta.xml file found!');
+		console.log(`Watching changes for "${resourceName}" resource.`);
 
-        chokidar
-            .watch('.', options)
-            .on('all', (event, path) => {
-                console.log("cos sie zrobilo");
-            });
-    });
+		chokidar
+			.watch('.', options)
+			.on('all', async (event, path) => {
+				try {
+					console.log(`Restarting "${resourceName}"`);
+					await axios.post('http://127.0.0.1:22005/resource-restart/call/restart', [resourceName]);
+				} catch (error) {
+					console.error(error.message);			
+				}
+			});
+	});
 
 program.parse(process.argv)
 
